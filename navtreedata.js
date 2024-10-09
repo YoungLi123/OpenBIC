b20409e8fa3e2349469d988ca4ece4245c97e8e2
YoungLi123 <YoungLi123@users.noreply.github.com>
2024-10-09 01:06:00 +0000

    Deploying to gh-pages from @ YoungLi123/OpenBIC@d7e782e2391749a8e0874148f6be1e6d30dea614 🚀

---
+++ navtreedata.js
@@ -0,0 +1,153 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "OpenBIC", "index.html", [
+    [ "README", "md_common_service_mctp_README.html", null ],
+    [ "Todo List", "todo.html", null ],
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "File Members", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "Typedefs", "globals_type.html", "globals_type" ],
+        [ "Enumerations", "globals_enum.html", "globals_enum" ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"aalc-rpu_2src_2ipmi_2include_2plat__ipmi_8h.html",
+"aalc-rpu_2src_2platform_2plat__gpio_8h.html#a7339eb37a7a6d4c5708f1ae1471d8bf0",
+"aalc-rpu_2src_2platform_2plat__sdr__table_8h.html#a4621c6d68e2c00657be61eed69eafa64",
+"aalc-rpu_2src_2platform_2plat__sensor__table_8h.html#ae87f509342d30a2cae5120ba23421013",
+"altera_8h.html#a325410dd8695d976755f6c073bee48ff",
+"at-cb_2src_2platform_2plat__class_8c.html#a71a0fbcc3a117fcba0b1d42751243323",
+"at-cb_2src_2platform_2plat__dev_8c.html#aa72bf850d7fdcc61c77c702b24574e3a",
+"at-cb_2src_2platform_2plat__fru_8h.html#ad9d175fb10f779b09415ea6e1e19c8f9a979ba17fcb1f6dceadce3334c9a57277",
+"at-cb_2src_2platform_2plat__i2c_8h.html#aee2773dd9ff29f1aaf7ab8eb3a99aff3",
+"at-cb_2src_2platform_2plat__pldm__monitor_8h.html#ab45b82e193f4891ab9c33002fab3b8e0a321c021d00371bf2dcf3b1ed95a5a30c",
+"at-cb_2src_2platform_2plat__sensor__table_8h.html#a34f23c99f393fe5a08cfff7db165fb1c",
+"at-mc_2src_2ipmi_2include_2plat__ipmi_8h.html#acd11096a52a192bd70e330d3be4b565aa40d8a1133c5d288046d10722d51306a9",
+"at-mc_2src_2platform_2plat__fru_8h.html#abb9589e57d74740b20fb3fee7f80a072",
+"at-mc_2src_2platform_2plat__init_8c.html#a6dcbc61a48750abf717e6a18c412b0ba",
+"at-mc_2src_2platform_2plat__sensor__table_8h.html#a246d40dd8dce99c136cab09eab722614",
+"dir_1b7335345bb23fdbb044710e8f661837.html",
+"functions_m.html",
+"gt-cc_2src_2ipmi_2plat__ipmi_8c.html#a5f13f7336a400bb177335e089e4e160e",
+"gt-cc_2src_2platform_2plat__hook_8c.html#ada8246e105a0cf074af2995dd3f513f7",
+"gt-cc_2src_2platform_2plat__pldm__monitor_8c.html#a1ed1a838a6383ee49678d42a743f24d9",
+"gt-cc_2src_2platform_2plat__pldm__monitor_8h.html#abc418aac84c9fda2756b2765c0e2ef0daaaa21673bba5f8c952fb534a14ce71a7",
+"gt-cc_2src_2platform_2plat__sensor__table_8h.html#af5b41283a44a13d3e01881efc488f035",
+"hal__i3c_8h.html#ae15af3543e811416d1108a4d24d3adf2",
+"ina238_8h.html#aab875d293868105fa46107f9bd3b1800a5ca0c7b8fc5cb646d6420da688c61ac0",
+"ipmi_8c.html#a60c7786b4145a3a7ca2424efa8d5da23",
+"libipmi_8h.html#a70b353ebe856e7c851f5b1b9f8da4863",
+"m88rt51632_8c.html#a8c747ba5323d6abbb55cc45009240a6e",
+"minerva-ag_2src_2platform_2plat__class_8h.html#aa62e1eef7ad87eb9edb5cc9a25006b70a1c755cebde3efeb54013fefed7caef17",
+"minerva-ag_2src_2platform_2plat__mctp_8c.html#aae74f7007ee726358eb20156b7809271",
+"minerva-ag_2src_2platform_2plat__pldm__sensor_8h.html#ad82fdca6088e33b3a3b57765f409a710",
+"mp5990_8c.html#ae141fa6e23a69c0c510d2d1da73b809c",
+"oem__1s__handler_8c.html#a04ff5e693ed4d8fb0d389cfba5df7931",
+"op2-op_2src_2platform_2plat__class_8h.html#a0630da2bad1091fb4145ff1c5e4968afa8defc927a0ba01e18061e13d1bc6a8df",
+"op2-op_2src_2platform_2plat__isr_8h.html#a0ab346a0c0b3687792ba9e160df99a72",
+"op2-op_2src_2platform_2plat__power__seq_8h.html#aced57420df5c1cedc36aa47769eb7eb8ae3749716f21b85a158e5165f2a8c3e42",
+"p3h284x_8c.html#ad883687b2a9932241578e6e2e41d1065",
+"plat__cpu_8h_source.html",
+"plat__modbus_8h.html#a190eee6d2e40808a751a33c197b09909",
+"plat__modbus_8h.html#af0a9670eebac0f8654c88282038cd1d5",
+"plat__threshold_8c.html#ab07dfa2a3d40b0c8d3c98d4d735066b4a4f924db80395fb76be633b94370231e2",
+"pldm__firmware__update_8h.html#a48027963ced01773fe869a3b7acc8e62aae34e1d7e7e70e0e085c0c0744d26fa2",
+"pldm__monitor_8h.html#a00dddbd3076a9a54d7f78b1efadc75a4",
+"pldm__oem_8c.html#a7cbd74ad36391bd5346c643f59008a8c",
+"pmbus_8h.html#a12523844811b20c9b0ff27f7ab1828f2a602e4d4d458a6eacf6e2c52433318270",
+"raa229621_8c.html#ad7a29a7a36f242711e57e6b04bee7bcd",
+"sdr_8h.html#a79caddf26c813de9858c5efb0899e7ae",
+"sensor_8c.html#ae64cc370281e8dd35571a37395f8574a",
+"sensor__shell_8h.html#a3cb98dee0f233909533ee75af1499195",
+"struct__SDR__Full__sensor__.html#a422833dea6e3c9ba326251fdd5254477",
+"struct__cxl__mctp__info.html#a3c73ecbab8662358a12b93529c74b1fe",
+"struct__ipmi__cmd__resp.html#abf461572cb7cd611c5191e34ccdb2040",
+"struct__nct7363__init__arg.html#a3dafc9be267bb5fc258a13dd4767869b",
+"struct__sq52205__init__arg__.html#af3bddbc7ea3ecf6cd0d2afeffbf96433",
+"structlattice__update__config.html#a13d8969f2fb53eeb96f7d8f7207bfab6",
+"structpldm__fw__update__param.html",
+"structpldm__update__component__resp.html#ae997fed0b377330b0c1e3707355a59c2",
+"tps53689_8c.html#a6ffb3ddcb678921c394c3877aabb6875",
+"wc-mb_2src_2lib_2plat__sys_8h_source.html",
+"wc-mb_2src_2platform_2plat__pmic_8h.html#a94b3688454a245a1b9e4b4937635f2cf",
+"xdpe15284_8h.html#a08456f392305cd954cbfc153f6234621",
+"yv3-dl_2src_2platform_2plat__class_8h.html#ac45edff48412880dc6ec52d810e5d628",
+"yv3-dl_2src_2platform_2plat__isr_8c.html#a28a50bb5f0313392301ab1b679112cce",
+"yv3-vf_2src_2platform_2plat__class_8h.html#af66fedc722e9e8c84c7563940e2ef678",
+"yv3-vf_2src_2platform_2plat__power__seq_8c.html#a49cf74274b225bc8be1e489cb4baae4e",
+"yv35-bb_2src_2lib_2plat__sys_8c.html#a7d7107eb5d6c2772420749c0eb4f61d3",
+"yv35-bb_2src_2platform_2plat__version_8h.html#a8755adc33637a29ad0905a5c6cfc535f",
+"yv35-cl_2src_2platform_2plat__class_8h.html#ae25ed9d7244e7426fe583e577f443851a8ed45514696686b8e9b5f7d1eba79461",
+"yv35-cl_2src_2platform_2plat__gpio_8h.html#a8b5e000590dc9026478feee216d12f81",
+"yv35-cl_2src_2platform_2plat__mctp_8h.html#aefdff4820fa5f80ff1d1d6308b546036",
+"yv35-gl_2src_2lib_2plat__sys_8h_source.html",
+"yv35-gl_2src_2platform_2plat__dimm_8c.html",
+"yv35-gl_2src_2platform_2plat__i2c_8h.html#ab1c81a3d460dd588d2a586bb890a7115",
+"yv35-gl_2src_2platform_2plat__sensor__table_8h.html#acc750c88c42088f65155e365afedbffd",
+"yv35-hd_2src_2platform_2plat__class_8h.html#aa384c01269dcb63aea106ff8e1432b1ca875e4b554ab9d518229a09a7fc086202",
+"yv35-hd_2src_2platform_2plat__hook_8h.html#a0a915f76fbe2ebc23da68a97eeac1afd",
+"yv35-hd_2src_2platform_2plat__sensor__table_8h.html#ad32449b7b2929b6406728ebb32e384f4",
+"yv35-hda1_2src_2platform_2plat__class_8h.html#ac45edff48412880dc6ec52d810e5d628a949af8ef1d92b278553d3b148c561c83",
+"yv35-hda1_2src_2platform_2plat__ipmb_8c.html#a73a183364bd3dce03b09dcd98f226233",
+"yv35-hda1_2src_2platform_2plat__sensor__table_8h.html#ab75a0a8a49626923b0c5193c090f0b9e",
+"yv35-ji_2src_2platform_2plat__class_8h.html#aa384c01269dcb63aea106ff8e1432b1cae3f005cdedaa1dc559457f3c9ae93530",
+"yv35-ji_2src_2platform_2plat__guid_8h.html",
+"yv35-ji_2src_2platform_2plat__power__status_8c.html",
+"yv35-nf_2src_2platform_2plat__hook_8c.html#a6f641981c13e541190a80dd365d78b09",
+"yv35-rf_2src_2lib_2plat__spi_8c.html#a2687bd6698a842e90092f1ac01b86e3c",
+"yv35-rf_2src_2platform_2plat__power__seq_8c.html#ad4d1d53229aa945fa01fa76960f7405a",
+"yv4-ff_2src_2platform_2plat__class_8c.html#afe186fac51a057492906d49592f2046c",
+"yv4-ff_2src_2platform_2plat__pldm__monitor_8h.html#a493d2b9b5340c425b804245a309be263afa73a00d8e4883901e18648c9d1b4a5a",
+"yv4-sd_2src_2ipmi_2plat__ipmi_8c.html#a1054e40675d4035541107c55aa4bb9c7a44f8390d88d4d1419bccc514338660e4",
+"yv4-sd_2src_2platform_2plat__def_8h.html#aab5d882172b304d6330a18885356e370",
+"yv4-sd_2src_2platform_2plat__hook_8h.html#adcad93504ed009532e7953d850b77ad7",
+"yv4-sd_2src_2platform_2plat__pldm__fw__update_8c.html#aa6d04537e3cd8566799b2ab8691cf18fa5bc691689cb8fb80f94d01646354d9d5",
+"yv4-wf_2src_2platform_2plat__class_8h.html#ad6c15268cfa0c5ea3583d4c1632ef410a60c0a6f187fa8f6066929daecc544e30",
+"yv4-wf_2src_2platform_2plat__mctp_8c.html#ad0897811b615a8001762d2bf90340092",
+"yv4-wf_2src_2platform_2plat__power__seq_8c.html#addc897ebd3f02df80a6fe01c145440a0"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
